9fadfdf32615c146f77a377b694aedf4ac4437d2
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-11-18 18:02:01 +0100

    rename all fields in the config file

--- convertconfig.py
+++ convertconfig.py
@@ -1,8 +1,10 @@
 import argparse
+import os
+
 from transfersrc import find_yaml, parser_yaml
 
 parser = argparse.ArgumentParser(description='Run the dlc projects.')
-parser.add_argument('--path', metavar='path', type=str, default='analyzed_videos',
+parser.add_argument('--path', metavar='path', type=str, default='TEST-Alex-2019-11-04',
                     help='path to project transfer')
 args = parser.parse_args()
 projectpath = args.path
@@ -14,4 +16,14 @@ import deeplabcut as d
 print('done')
 config = parser_yaml(config_path)
 config['project_path'] = projectpath
+print("ppppa: ",os.path.abspath(projectpath))
+new_proj_path = os.path.abspath(projectpath)
+
+# defines a new set of videos with the project path:
+video_sets_new = {}
+for k,v in config['video_sets'].items():
+    video_sets_new[os.path.join(new_proj_path,os.path.basename(k))] = v
+
+config['video_sets']= video_sets_new
 d.auxiliaryfunctions.write_config(config_path, config)
+print(config)
