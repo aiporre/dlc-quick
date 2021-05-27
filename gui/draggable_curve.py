899a81134eacbe2cdd71aaa6c8e20c7881426ac1
Ariel <ariel.iporre.rivas@gmail.com>
2021-05-27 13:00:26 +0200

    implement print fancy string draggable curves

--- gui/draggable_curve.py
+++ gui/draggable_curve.py
@@ -182,11 +182,12 @@ class DraggableCurve:
                 self.curve_patch.set_xy(self.new_point_set)
                 self.curve_patch.figure.canvas.draw()
 
-
+    def __str__(self):
+        return str(self.point_set[0]) + ' --> ' + str(self.point_set[-1])
 
 
 if __name__ == '__main__':
-    geometry = [[0.0, 0.0], [0.1, 0.05], [0.2, 0.15], [0.3, 0.20], [0.4, 0.25], [0.5, 0.30],
-                         [0.6, 0.25], [0.7, 0.15], [0.8, 0.05], [0.9, 0.025], [1.0, 0.5]]
-    dc = DraggableCurve(geometry)
+    points = [[0.0, 0.0], [0.1, 0.05], [0.2, 0.15], [0.3, 0.20], [0.4, 0.25], [0.5, 0.30],
+              [0.6, 0.25], [0.7, 0.15], [0.8, 0.05], [0.9, 0.025], [1.0, 0.5]]
+    dc = DraggableCurve(points)
     plt.show()
