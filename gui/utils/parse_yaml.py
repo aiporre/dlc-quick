8bee74d18722ba2dd616e79846f7bb3afaf72259
Ariel <ariel.iporre.rivas@gmail.com>
2021-06-01 16:34:27 +0200

    Implement write default/input config training whisking model.yaml

--- gui/utils/parse_yaml.py
+++ gui/utils/parse_yaml.py
@@ -1,4 +1,6 @@
 import yaml
+from ruamel.yaml import YAML
+
 
 def parse_yaml(filepath):
     with open(filepath, 'r') as stream:
@@ -9,3 +11,37 @@ def parse_yaml(filepath):
             return content
         except yaml.YAMLError as exc:
             print(exc)
+def create_whisking_config_template():
+    yaml_str = """\
+        # Dataset parameters
+            enable_eager:
+            enable_last_channel:
+            image_dim_width:
+            image_dim_height:
+            \n
+        # Dataset optimization
+            cache:
+            shuffle_buffer:
+            split_rate:
+            \n
+        # Training parameters
+            learning_rate:
+            display_iter:
+            batch_size:
+            init_weights:
+            """
+    ruamelFile = YAML()
+    cfg_file = ruamelFile.load(yaml_str)
+
+    return cfg_file, ruamelFile
+
+
+def write_whisking_config(configname, cfg):
+
+    with open(configname, "w") as cf:
+        cfg_file, ruamelFile = create_whisking_config_template()
+
+        for key in cfg.keys():
+            cfg_file[key] = cfg[key]
+
+        ruamelFile.dump(cfg_file, cf)
