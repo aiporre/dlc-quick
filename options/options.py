9ba8932e4a8ef8ea59520f4e4cb8caec41e64f9f
Ariel <ariel.iporre.rivas@gmail.com>
2021-06-15 11:18:56 +0200

    fix sloppy behavior printing caused by OptionsPad single class.
    The bug was that the first option was not displayed. Fixed by trimming text before printing. I duno why :)

--- options/options.py
+++ options/options.py
@@ -43,11 +43,10 @@ class Option:
         # prints without colors
         # TODO: 2 is hardcoded to the lenght of the cursor
         if not curses.has_colors():
-            self.screen.addstr(self.row, 2, self.text)
+            self.screen.addstr(self.row, 2, self.text + ' ysize = ' + str(self.screen.getyx()[0]))
         # prints with colors
         else:
-            self.screen.addstr(self.row, 2, self.text, curses.color_pair(self.status))
-        self.screen.refresh()
+            self.screen.addstr(self.row, 2, ' ' + self.text.strip(), curses.color_pair(self.status))
 
 
 def print_options(options):
@@ -56,32 +55,61 @@ def print_options(options):
 
 
 class Pad:
-    def __init__(self, screen, shift_y, shift_x, rate_height, rate_width):
+    def __init__(self, screen, pheight, shift_y, shift_x, sheight, swidth):
         self.screen = screen
         self.shift_y = shift_y
         self.shift_x = shift_x
-        height, width = screen.getmaxyx()
-        self.pad = curses.newpad(rate_height*height, rate_width*width)
+        self.pheight, self.pwidth = (pheight, swidth)
+        self.sheight, self.swidth = (sheight, swidth)
+        self.pheight = pheight
+        self.pad = curses.newpad(pheight, swidth)
+        self.screen.refresh()
         self.pad.scrollok(True)
         self.pad_pos = 0
 
     def refresh(self):
-        height, width = self.screen.getmaxyx()
-        self.pad.refresh(self.pad_pos + 2, 0, 0, 0, height - 1, width)
+        self.pad.refresh(self.pad_pos, 0, 0, 0, self.sheight - 1, self.swidth)
 
-    def moveup(self):
-        pass
+    def move_up(self):
+        self.pad_pos = max(self.pad_pos-1, 0)
 
-    def movedown(self):
-        pass
+    def move_down(self):
+        self.pad_pos = min(self.pad_pos + 1, self.pheight - 1)
 
-class OptionPad(Pad):
-    def __init__(self, screen, options_text, shift_y, shift_x, rate_height, rate_width):
-        super(OptionPad).__init__(screen, shift_y, shift_x, rate_height, rate_width)
+
+class OptionsPad(Pad):
+    def __init__(self, screen, options_text, shift_y, shift_x, sheight, swidth):
+        super().__init__(screen, pheight=len(options_text), shift_y=shift_y, shift_x=shift_x, sheight=sheight, swidth=swidth)
         self.options_text = options_text
         self.options = [Option(self.pad, option, i) for i, option in enumerate(self.options_text)]
-        print_options(self.options)
+        self.cursor_text = "➤➤"
+        self.cursor_position = 0
 
+    def show(self):
+        print_options(self.options)
+        self.pad.addstr(self.cursor_position, 0, self.cursor_text)
+        self.refresh()
+
+    def toogle_selection(self):
+        self.options[self.cursor_position].toogle_selection()
+        print_options(self.options)
+        self.refresh()
+
+    def move_down(self):
+        self.pad.addstr(self.cursor_position, 0, ''.join(len(self.cursor_text)*[' ']))
+        self.cursor_position = min(self.cursor_position + 1, self.pheight - 1)
+        self.pad.addstr(self.cursor_position, 0, self.cursor_text)
+        if self.cursor_position - self.pad_pos > self.sheight-1:
+            super().move_down()
+        self.refresh()
+
+    def move_up(self):
+        self.pad.addstr(self.cursor_position, 0, ''.join(len(self.cursor_text)*[' ']))
+        self.cursor_position = max(self.cursor_position-1, 0)
+        self.pad.addstr(self.cursor_position, 0, self.cursor_text)
+        if self.cursor_position < self.pad_pos:
+            super().move_up()
+        self.refresh()
 
 class OptionInterface:
     def __init__(self, options_text: str):
@@ -94,35 +122,27 @@ class OptionInterface:
             curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_GREEN)
             curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_WHITE)
             curses.init_pair(3, curses.COLOR_RED, curses.COLOR_GREEN)
-
+        height, width = screen.getmaxyx()
+        options_pad = OptionsPad(screen, self.options_text, shift_y=0, shift_x=0, sheight=height, swidth=width)
         screen.refresh()
-        try:
-            options = [Option(screen, option, i) for i, option in enumerate(self.options_text)]
-            print_options(options)
-            cursor_row = 0
-            screen.addstr(cursor_row, 0, "➤➤")
-            screen.refresh()
 
+        try:
+            options_pad.show()
             while 1:
                 c = screen.getch()
                 if c == ord('D') or c == ord('d'):
-                    screen.addstr(cursor_row, 0, "  ")
-                    cursor_row = min(cursor_row + 1, len(options) - 1)
-                    screen.addstr(cursor_row, 0, "➤➤")
-                if c == ord('A') or c == ord('a'):
-                    screen.addstr(cursor_row, 0, "  ")
-                    cursor_row = max(cursor_row - 1, 0)
-                    screen.addstr(cursor_row, 0, "➤➤")
-                if c == 32:
-                    options[cursor_row].toogle_selection()
-                    print_options(options)
+                    options_pad.move_down()
+                elif c == ord('A') or c == ord('a'):
+                    options_pad.move_up()
+                elif c == 32:
+                    options_pad.toogle_selection()
                 screen.refresh()
 
         except Exception as e:
             print(e)
             # curses.napms(2000)
             screen.getch()
-
+            raise e
 
         finally:
             curses.endwin()
@@ -132,10 +152,7 @@ class OptionInterface:
 
 
 if __name__ == '__main__':
-    example_options = [' option 1 ',
-                    ' option 2 ',
-                    ' option 3 ',
-                    ' option 4 ',
-                    ' option 5 ',
-                    ' option 6 ']
+    example_options = [f' OPTION {i}' for i in range(10)]
+
+
     OptionInterface(example_options).launch()
